8c40ad9d6fe38dc6893093d55bf67889f601ec1d
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-09-07 12:24:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,28 +9,6 @@ server.get('/', (req, res)=>{
   res.send('ROOT FILE')
 })
 
-server.get('/hobbits', (req, res) => {
-  res.send('Welcome to Hobbiton');
-})
-
-server.post('/hobbits', (req, res) => {
-  res.status(201).json({
-    url: '/hobbits',
-    operation: 'POST'
-  })
-})
-
-server.put('/hobbits', (req, res) => {
-  res.status(200).json({
-    url: '/hobbits',
-    operation: 'PUT'
-  })
-})
-
-server.delete('hobbits', (req, res) => {
-  res.status(204);
-})
-
 server.listen(port, ()=> {
   console.log(`Server listening on port ${port}`)
 })
